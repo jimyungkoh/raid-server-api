a284a0948ac70c8b0b6bbfe739b814ebc7ff0b66
Jimyung Koh <kojimyung1206@naver.com>
2022-09-16 13:26:25 +0900

    style: prettier, eslint 규칙 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,14 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off','no-var': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+        'singleQuote': true,
+        'semi': true
+      }
+    ]
   },
 };
