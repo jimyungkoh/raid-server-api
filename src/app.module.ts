c1c230d0f78dddbaaee13b7982added0a3c7804d
Jimyung Koh <kojimyung1206@naver.com>
2022-09-17 20:05:18 +0900

    feat: /boss-raid 관련 리소스 추가
    ## feat: /boss-raid 관련 리소스 추가
    - boss-raid 관련 리소스(service, entity, module, controller) 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MySQLModule } from './database/MySQLModule';
+import { BossRaidModule } from './boss-raid/boss-raid.module';
 
 @Module({
-  imports: [MySQLModule, UserModule],
+  imports: [MySQLModule, UserModule, BossRaidModule],
   controllers: [AppController],
   providers: [AppService],
 })
